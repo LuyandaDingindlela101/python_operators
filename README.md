3c88bf6be4eb4fdc1d82e36c84f3cb17048556d9
LuyandaDingindlela101 <3995219@myuwc.ac.za>
2021-04-15 11:08:54 +0200

    renamed variable min_number to max_number and added a README.md file

---
+++ README.md
@@ -0,0 +1,7 @@
+# TASK 1
+   - Write a Python program to get the third side of right angled triangle from two given sides.
+   - Your program should allow user to input the size of two sides and calculate the other side.
+   - Also calculate the area and display the answer in binary format.
+   
+ #   TASK 2
+   - Write a program to get the largest number from a list.
--- task2.py
+++ task2.py
@@ -1,17 +1,17 @@
 #   TASK 2
 #   Write a program to get the largest number from a list.
-min_number = 0
+max_number = 0
 my_numbers = [5,1,3,2,4]
 
 #   WE USE THE FOR LOOP TO RUN THROUGH EACH ITEM(NUMBER) IN THE LIST AND COMPARE IT TO THE max
 for current_number in my_numbers:
     #   CHECK IF THE current_number IS LESS THAN THE max
-    if current_number > min_number:
+    if current_number > max_number:
         #   IF IT IS, THEN THAT MUST BE THE LOWEST NUMBER SO FAR...UP UNTIL THERE ARE NO MORE NUMBERS
-        min_number = current_number
+        max_number = current_number
 
 #   PRINT THE max
-print("The lowest number in the list is: " + str(min_number))
+print("The lowest number in the list is: " + str(max_number))
 
 #   2. Given a list of numbers with duplicates, how would you remove the duplicates so that the list become unique.
 new_list = []
